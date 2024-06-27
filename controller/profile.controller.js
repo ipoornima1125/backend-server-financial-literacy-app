0a0981196ad6fe405e82372701857bb85881dc4e
ipoornima1125 <Poornima2004.11@rediffmail.com>
2024-06-27 23:25:25 +0530

    fixed errors

--- controller/profile.controller.js
+++ controller/profile.controller.js
@@ -3,7 +3,7 @@ import User from '../model/user.model.js';
 
 const getUserProfile = async (req, res) => {
   try {
-    const userEmail = req.body.email; 
+    const userEmail = req.userEmail; 
     const user = await User.findOne({email:userEmail});
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
