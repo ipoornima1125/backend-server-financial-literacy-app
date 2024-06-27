9c18188d01b73b7d706fca9d46081aa608ffb096
ipoornima1125 <Poornima2004.11@rediffmail.com>
2024-06-27 16:38:02 +0530

    authentication succesfully don eusing middleware

---
+++ middleware/authenticate.token.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+
+import User from '../model/user.model';
+
+
+const authenticateToken = async (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) {
+        return res.sendStatus(401);
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const user = await User.findById(decoded.userId);
+
+        if (!user) {
+            return res.sendStatus(401);
+        }
+
+        req.user = user;
+        next();
+    } catch (err) {
+        console.error("Error:", err);
+        return res.sendStatus(403);
+    }
+};
+
+export default authenticateToken;
--- route/profile.route.js
+++ route/profile.route.js
@@ -1,7 +1,7 @@
 import getUserProfile from "../controller/profile.controller.js";
-
+import authenticateToken from "../middleware/authenticate.token.js";
 import express from 'express';
 const router=express.Router();
-router.get("/",getUserProfile);
+router.get("/",authenticateToken,getUserProfile);
 
 export default router;
